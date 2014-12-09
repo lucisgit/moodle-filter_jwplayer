84af28823f6ae612c748385bba024905ffccd108
Johannes Burk <mail@johannesburk.de>
2014-12-09 12:40:53 +0100

    fixed indention

--- module.js
+++ module.js
@@ -5,17 +5,17 @@ M.filter_jwplayer.init = function(Y, playerid, setupdata) {
 };
 
 M.filter_jwplayer.addButton = function(Y, playerid, img, tttext) {
-	jwplayer(playerid).addButton(
-		//This portion is what designates the graphic used for the button
-		img,
-		//This portion determines the text that appears as a tooltip
-		tttext, 
-		//This portion designates the functionality of the button itself
-		function() {
-			//With the below code, we're grabbing the file that's currently playing
-			window.location.href = jwplayer().getPlaylistItem()['file'] + '?forcedownload=true';
-		},
-		//And finally, here we set the unique ID of the button itself.
-		"download"
-	);
+    jwplayer(playerid).addButton(
+        //This portion is what designates the graphic used for the button
+        img,
+        //This portion determines the text that appears as a tooltip
+        tttext, 
+        //This portion designates the functionality of the button itself
+        function() {
+            //With the below code, we're grabbing the file that's currently playing
+            window.location.href = jwplayer().getPlaylistItem()['file'] + '?forcedownload=true';
+        },
+        //And finally, here we set the unique ID of the button itself.
+        "download"
+    );
 };
