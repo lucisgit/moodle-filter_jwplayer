c09fd71bf8afae2ac37beafb49d9b95a7b5f0369
Johannes Burk <me@jojoob.de>
2015-04-28 14:32:51 +0200

    filter_flowplayer: Move download button setup in jwplayer init call

--- module.js
+++ module.js
@@ -1,12 +1,12 @@
 M.filter_jwplayer = {};
 
-M.filter_jwplayer.init = function(Y, playerid, setupdata) {
+M.filter_jwplayer.init = function(Y, playerid, setupdata, downloadbtn) {
     jwplayer(playerid).setup(setupdata);
-};
 
-M.filter_jwplayer.add_button = function(Y, playerid, img, tttext) {
-    jwplayer(playerid).addButton(img, tttext, function() {
-            // Grab the file that's currently playing.
-            window.location.href = jwplayer(playerid).getPlaylistItem().file + '?forcedownload=true';
+    if (downloadbtn !== undefined) {
+        jwplayer(playerid).addButton(downloadbtn.img, downloadbtn.tttext, function() {
+                // Grab the file that's currently playing.
+                window.location.href = jwplayer(playerid).getPlaylistItem().file + '?forcedownload=true';
             }, "download");
+    }
 };
