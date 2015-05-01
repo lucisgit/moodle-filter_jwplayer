e031daf995af6b0c0f45d648a1754cb744f99cf4
Johannes Burk <me@jojoob.de>
2015-05-01 10:50:58 +0200

    filter_jwplayer: fix 'Cannot read property 'img' of null' js warning

--- module.js
+++ module.js
@@ -3,7 +3,7 @@ M.filter_jwplayer = {};
 M.filter_jwplayer.init = function(Y, playerid, setupdata, downloadbtn) {
     jwplayer(playerid).setup(setupdata);
 
-    if (downloadbtn !== undefined) {
+    if (downloadbtn != undefined) {
         jwplayer(playerid).addButton(downloadbtn.img, downloadbtn.tttext, function() {
                 // Grab the file that's currently playing.
                 window.location.href = jwplayer(playerid).getPlaylistItem().file + '?forcedownload=true';
