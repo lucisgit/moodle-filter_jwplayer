829e6c7e9d1b542fe4ca7b0222124aaa60851d4c
Johannes Burk <me@jojoob.de>
2015-06-17 11:45:22 +0200

    Declare and rename executenamespacedFunction outside init function

--- module.js
+++ module.js
@@ -3,19 +3,9 @@ M.filter_jwplayer = {};
 M.filter_jwplayer.init = function(Y, playerid, setupdata, buttons) {
     jwplayer(playerid).setup(setupdata);
 
-    var executenamespacedFunction = function(namespacedFunctionName, args) {
-        var namespaces = namespacedFunctionName.split(".");
-        var fn = namespaces.pop();
-        var context = window;
-        for (var i = 0; i < namespaces.length; i++) {
-            context = context[namespaces[i]];
-        }
-        context[fn].apply(context, args);
-    };
-
     function btncallback(i) {
         return function() {
-            executenamespacedFunction(buttons[i].callback, [playerid]);
+            M.filter_jwplayer.execute_namespaced_function(buttons[i].callback, [playerid]);
         };
     }
 
@@ -26,4 +16,14 @@ M.filter_jwplayer.init = function(Y, playerid, setupdata, buttons) {
 
 M.filter_jwplayer.download = function(playerid) {
     window.location.href = jwplayer(playerid).getPlaylistItem().file + '?forcedownload=true';
+};
+
+M.filter_jwplayer.execute_namespaced_function = function(namespacedfunctionname, args) {
+    var namespaces = namespacedfunctionname.split(".");
+    var fn = namespaces.pop();
+    var context = window;
+    for (var i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]];
+    }
+    context[fn].apply(context, args);
 };
