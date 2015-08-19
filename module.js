4cb75d048b40f527982283c3118503df7abc1c8f
Owen Barritt <obarritt@wset.co.uk>
2015-08-19 18:15:24 +0100

    filter_jwplayer:  Change JWPlayer JS API calls to a reference to the player
    In line with best practice guidance at http://support.jwplayer.com/customer/portal/articles/1413074-javascript-api-quick-start

--- module.js
+++ module.js
@@ -1,12 +1,13 @@
 M.filter_jwplayer = {};
 
 M.filter_jwplayer.init = function(Y, playerid, setupdata, downloadbtn) {
-    jwplayer(playerid).setup(setupdata);
+    var playerinstance = jwplayer(playerid);
+    playerinstance.setup(setupdata);
 
     if (downloadbtn != undefined) {
         jwplayer(playerid).addButton(downloadbtn.img, downloadbtn.tttext, function() {
                 // Grab the file that's currently playing.
-                window.location.href = jwplayer(playerid).getPlaylistItem().file + '?forcedownload=true';
+                window.location.href = playerinstance.getPlaylistItem().file + '?forcedownload=true';
             }, "download");
     }
 };
